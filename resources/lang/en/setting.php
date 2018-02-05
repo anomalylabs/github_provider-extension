8cd7f2890d63e51b44e757d9bd409f20128aec81
Ryan Thompson <service@aiwebsystems.com>
2018-02-05 12:56:59 -0600

    Lang

--- resources/lang/en/setting.php
+++ resources/lang/en/setting.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    'client_id'     => [
+    'client_id'         => [
         'name' => 'Client ID',
     ],
-    'client_secret' => [
+    'client_secret'     => [
         'name' => 'Client Secret',
     ],
     'scope'             => [
         'name'         => 'Scopes',
-        'instructions' => 'Specify the <a href="https://developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps/#available-scopes" target="_blank">user scopes</a> for Facebook.',
+        'instructions' => 'Specify the <a href="https://developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps/#available-scopes" target="_blank">user scopes</a> for GitHub.',
     ],
     'application_scope' => [
         'name'         => 'Application Scopes',
-        'instructions' => 'Specify the <a href="https://developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps/#available-scopes" target="_blank">application scopes</a> for Facebook.',
+        'instructions' => 'Specify the <a href="https://developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps/#available-scopes" target="_blank">application scopes</a> for GitHub.',
     ],
 ];
